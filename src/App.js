666647d2d8062f35c1ae31eff76f6eb2992db6d7
muhammadsajit <sajit.du.niter@gmail.com>
2022-03-26 13:57:25 +0600

    first question is added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Shop from './Components/Shop/Shop';
+import Question from './Components/Question/Question';
 
 function App() {
   return (
     <div className="App">
         <Header></Header>
         <Shop></Shop>
+        <Question></Question>
         
      </div>
   );
---
+++ src/Components/Question/Question.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Question = () => {
+    return (
+        <div>
+            <div>
+              <h1>How to react works</h1>
+              <p>When we create a component in react.js then it show the actual Document Object Model in UI.Constiquently react create Virtual DOM and keeps all the component in it.When we change any component/state/data then react create new Virtual DOM.Then react compare the new Virtual DOM with the previous Virtual DOM.React do this for the check where data or state or component changes.By this way react Update the changes by diff Alogorithm and show the changes component in UI.</p>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Question;
