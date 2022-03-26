9c7f9bbf44eb148b3de8a468e5ff0e89469685b4
muhammadsajit <sajit.du.niter@gmail.com>
2022-03-26 10:28:29 +0600

    image added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-info'>
+            <h1>Wholesale Buyers</h1>
+            <h2>Wants 4 Bags</h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {name,img,price}=props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+        </div>
+    );
+};
+
+export default Product;
